206b7c0c428e33f0cc2efefd9a476f7b5f514841
Mauricio123-bravo <hectormauriciobravo@gmail.com>
2025-10-29 11:22:58 -0500

    update

--- proyecto/Program.cs
+++ proyecto/Program.cs
@@ -13,6 +13,7 @@ namespace proyecto
             // Add services to the container.
             var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw 
                 new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
             builder.Services.AddDbContext<ApplicationDbContext>(options =>
             options.UseNpgsql(connectionString));
 
